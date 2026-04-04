09e860ceadb5cc5054214426e45549fae90b1924
Danny R <danny@erebus.cymru>
2026-04-04 21:38:40 +0000

    Refactored the code to accept command line arguments, conditional proxy line in the reqwest builder and added in slightly more graceful error handling.

--- emdeefive/rust/src/main.rs
+++ emdeefive/rust/src/main.rs
@@ -1,37 +1,71 @@
 use scraper::{Html, Selector};
 
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+     #[arg(short, long)]
+    target: std::net::SocketAddr,
+
+    #[arg(short, long)]
+    proxy: Option<std::net::SocketAddr>,
+}
+
+//Parses request bodies for the required element
+fn el_parse(body: &str, selector: &str) -> Option<String> {
+
+	let document = Html::parse_document(&body);
+    
+    let element = Selector::parse(selector).unwrap();
+
+    document
+        .select(&element)
+        .next()
+        .map(|el| el.text().collect::<String>())
+}
+
+fn create_hash(hash: &str) -> String {
+    format!("{:x}", md5::compute(hash.as_bytes()))
+}
+
 fn main() -> Result<(), reqwest::Error> {
 
-    let client = reqwest::blocking::Client::builder()
-    .cookie_store(true)
-    .proxy(reqwest::Proxy::http("http://127.0.0.1:8080")?)
-    .build()
-    .unwrap();
-    
-    let url = "http://154.57.164.75:31226";
+	let args = Cli::parse();
 
-    let body = client.get(url).send()?.text()?;
+    let mut builder = reqwest::blocking::Client::builder()
+    .cookie_store(true);
 
-    println!("{}", &body);
-
-    let document = Html::parse_document(&body);
-    
-    let h3 = Selector::parse("h3").unwrap();
-
-    let mut hash = None;
-
-    if let Some(element) = document.select(&h3).next() {
-        let text = element.text().collect::<String>();
-        println!("{}", text );
-        hash = Some(format!("{:x}", md5::compute(text.as_bytes())));
-        println!("{:?}", hash);
+    if let Some(proxy) = args.proxy {
+        builder = builder.proxy(reqwest::Proxy::http(format!("http://{}",proxy))?);
     }
 
+    let client = builder.build()?;
+    
+    let url = format!("http://{}", args.target);
+
+    let body = client.get(&url).send()?.text()?;
+    
+    let mut hash = None;
+
+    if let Some(text) = el_parse(&body, "h3") {
+        println!("Text to hash: {}", text );
+        hash = Some(create_hash(&text));
+        println!("MD5 Hash: {:?}", hash);
+    }
+
+    let Some(hash) = hash else {
+    println!("Nothing found to hash");
+    return Ok(());
+};
+
     let res = client.post(url)
-    .form(&[("hash", hash.unwrap())])    
+    .form(&[("hash", hash)])    
     .send();
 
-    println!("{}", res.unwrap().text().unwrap());
+    let res_body = res?.text()?;
+
+    let flag = el_parse(&res_body, "p");
+    println!("Flag: {}", flag.unwrap_or_else(|| "No flag, try again!".to_string()));
 
     Ok(())
 }
